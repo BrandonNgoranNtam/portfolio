3129f583ff9f88a4f15995ca9131dfb687a57c25
Brandon Ngoran Ntam <brandonzoom886@gmail.com>
2024-07-09 12:58:52 +1000

    added assets and finished implementing the bendogrid with animations

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -3,15 +3,24 @@ import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
 import { gridItems } from '@/data'
 
 const Grid = () => {
-  return (
-    <section id="about">
-        <BentoGrid>
-            {gridItems.map((item,i) => (
-                <BentoGridItem id={item.id} key = {item.id} title={item.title} description={item.description} />
-            ))}
-        </BentoGrid>
-    </section>
-  )
+    return (
+        <section id="about">
+            <BentoGrid className='w-full py-20'>
+                {gridItems.map(({ id, title, description, className, img, spareImg, titleClassName, imgClassName}) => (
+                    <BentoGridItem
+                        id={id}
+                        key={id}
+                        title={title}
+                        description={description}
+                        className={className} 
+                        img={img}
+                        imgClassName={imgClassName}
+                        titleClassName={titleClassName}
+                        spareImg={spareImg}/>
+                ))}
+            </BentoGrid>
+        </section>
+    )
 }
 
 export default Grid
