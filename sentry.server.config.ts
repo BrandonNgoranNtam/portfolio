d97742b8c65b53dcceb7573852253d4f6f3696b9
Brandon Ngoran Ntam <brandonzoom886@gmail.com>
2024-07-09 14:37:05 +1000

    Added recent projects section + sentry integration for bugs

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://9c2b3ff014bb4612baef96fcf3d4cd5c@o4507541154430976.ingest.de.sentry.io/4507569931157584",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
